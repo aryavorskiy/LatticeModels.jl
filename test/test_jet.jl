d1d47a5676b854738e98b01db3913155fd6df2f6
aryavorskiy <aleksandr.iavorskii@weizmann.ac.il>
2024-10-01 11:18:43 +0300

    Fix show JET report in tests

--- test/test_jet.jl
+++ test/test_jet.jl
@@ -9,9 +9,7 @@ import LinearAlgebra, ProgressMeter, KrylovKit
     )
     rep = report_package(LatticeModels; toplevel_logger=nothing,
         ignored_modules=jet_ignored_modules)
-    nreports = length(JET.get_reports(rep))
-    MAX_REPORTS = 17
-    (ENV["SHOW_JET_REPORT"] == "true" || nreports > MAX_REPORTS) && show_report(rep)
-    @test nreports <= MAX_REPORTS
+    print(rep)
+    @test length(JET.get_reports(rep)) <= 17
     @test_broken length(JET.get_reports(rep)) == 0
 end
